3fb277f166bdea5b35a11f6526960599294bd006
JoseMG-ux <joseutp14@gmail.com>
2021-03-27 21:12:10 -0600

    Agregar archivos de proyecto.

---
+++ PFinalTeleinf/Controllers/laboratorioDBController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc; 
+using PFinalTeleinf.Models;
+using PFinalTeleinf.Models.ViewModels;
+
+namespace PFinalTeleinf.Controllers
+{
+    public class laboratorioDBController : Controller
+    {
+        // GET: laboratorioDB
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lst;
+            using (laboratorioDBEntities db = new laboratorioDBEntities())
+            {
+                
+                 lst = (from d in db.CITA
+                           select new ListTablaViewModel
+                           {
+                                Id = d.ID_CITA,
+                                IdUser = (int)d.ID_USUARIO,
+                                NamePacient = d.NOMBREPACIENTE,
+                                ApellidoPacient = d.APELLIDO,
+                                CedulaPacient = d.CEDULA,
+                                DireccionPacient = d.DIRECCION,
+                                TelefonoPacient = d.TELEFONO,
+                                FechaNac = (DateTime)d.FECHANACIMIENTO,
+                                FechaPrCita = (DateTime) d.FECHAPROGRAMADA
+                           }).ToList();
+            }
+            return View(lst);
+        }
+    }
+}
---
+++ PFinalTeleinf/Models/ViewModels/ListTablaViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PFinalTeleinf.Models.ViewModels
+{
+    public class ListTablaViewModel
+    {
+        public int Id { get; set; } 
+        public int IdUser { get; set; }
+
+        public string NamePacient { get; set; }
+
+        public string ApellidoPacient { get; set; }
+
+        public string CedulaPacient { get; set; }
+
+        public string DireccionPacient { get; set; }
+
+        public string TelefonoPacient { get; set; }
+
+        public DateTime FechaNac { get; set; }
+
+        public DateTime FechaPrCita { get; set; }
+         
+    }
+}
